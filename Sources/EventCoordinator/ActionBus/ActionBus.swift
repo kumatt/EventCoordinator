c1b71c12380b36ca48b5f6b6239a57335b8da06a
kumatt <kuma.zhou@qq.com>
2025-08-05 11:45:39 +0800

    perf: 使用combine的信号量替代block回调，优化响应

--- Sources/EventCoordinator/ActionBus/ActionBus.swift
+++ Sources/EventCoordinator/ActionBus/ActionBus.swift
@@ -11,17 +11,12 @@ public enum ActionCenter { }
 /// 事件的中心处理
 @MainActor
 public final class ActionBus {
-    /// 目标对象
-    public typealias Object = Any
-
     /// 默认的事件管线（单例）
     /// note：也可自定义 独属于某个模块的ActionBus
     public static let `default` = ActionBus()
     
-    /// 事件的订阅
-    private var actionCancellables: [String: Set<Reducer>] = [:]
-    
-    public var cancellables = Set<AnyCancellable>()
+    /// 事件订阅的信号量集合
+    private var subjects: [String: Any] = [:]
     
     /// 初始化方法
     public init() { }
@@ -32,86 +27,28 @@ public extension ActionBus {
     /// 发送事件
     /// 当前事件存在Reducer订阅时，事件才会发送
     @discardableResult
-    func send<Action: Sendable>(_ action: Action, object: Sendable? = nil, file: String = #file, function: String = #function, line: Int = #line) async -> Bool {
+    func send<Action: Sendable>(_ action: Action, object: Sendable? = nil, file: String = #file, function: String = #function, line: Int = #line) -> Bool {
         
-        /// 记录事件为verbose，debug环境下
-//        Log.log(action, level: .verbose, file: file, function: function, line: line)
+        let key = String(reflecting: Action.self)
 
-        
-        guard let reducers = actionCancellables[String(reflecting: Action.self)],
-            !reducers.isEmpty else {
-            return false
+        if let subject = subjects[key] as? PassthroughSubject<Action, Never> {
+            subject.send(action)
+            return true
         }
-        for reducer in reducers {
-            await reducer(action)
-        }
-        return true
+        return false
     }
 }
 
 // MARK: - sink action
 public extension ActionBus {
-
-    /// 处理事件
-    /// - Parameter receiveValue: 事件的回调
-    /// - Returns: 订阅的生命周期
-    func sink<Action: Sendable>(receiveValue:@Sendable @escaping (Action) -> Void) -> AnyCancellable {
-        sink(with: Action.self, reducer: Reducer({ value in
-            guard let action = value as? Action else { return }
-            receiveValue(action)
-        }))
-    }
-}
-
-// MARK: - sink的最终实现
-private extension ActionBus {
-    /// 处理事件
-    /// 订阅某个事件，进行响应
-    private func sink<Action: Sendable>(with actionType: Action.Type, reducer: Reducer) -> AnyCancellable {
-        let hashable = String(reflecting: actionType)
-        var reducers = actionCancellables[hashable] ?? Set()
-        reducers.insert(reducer)
-        actionCancellables[hashable] = reducers
-        /// AnyCancellable被释放时，移除监听
-        return AnyCancellable {[weak self] in
-            Task { @MainActor [weak self] in
-                guard let self else { return }
-                var reducers = self.actionCancellables[hashable]
-                reducers?.remove(reducer)
-                self.actionCancellables[hashable] = reducers
-            }
+    func publisher<Action: Sendable>(for actionType: Action.Type) -> AnyPublisher<Action, Never> {
+        let key = String(reflecting: Action.self)
+        if let subject = subjects[key] as? PassthroughSubject<Action, Never> {
+            return subject.eraseToAnyPublisher()
+        } else {
+            let subject = PassthroughSubject<Action, Never>()
+            subjects[key] = subject
+            return subject.eraseToAnyPublisher()
         }
-    }
-}
-
-// MARK: - 事件监听的封装
-extension ActionBus {
-    struct Reducer: Sendable {
-        /// hash tag
-        private let id = UUID()
-        /// do anything
-        let block: @Sendable (Sendable) async -> Void
-        
-        @inline(__always)
-        init(_ block: @Sendable @escaping (Sendable) async -> Void) {
-            self.block = block
-        }
-        
-        @inline(__always)
-        func callAsFunction(_ action: Sendable) async {
-            await block(action)
-        }
-    }
-}
-
-// MARK: - Reducer is Hashable
-extension ActionBus.Reducer: Hashable {
-    // 实现 Hashable 协议
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
     }
 }
